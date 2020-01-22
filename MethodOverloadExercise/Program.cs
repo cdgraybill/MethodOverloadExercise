0f82a79f0af97ce5f81a279748e6e1ceda4c2119
Cameron Graybill <cdgraybill@gmail.com>
2020-01-22 13:46:25 -0600

    Add project files.

---
+++ MethodOverloadExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MethodOverloadExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = Add(0, 1, true);
+            Console.WriteLine(m);
+            Console.ReadLine();
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool x)
+        {
+            int answer = a + b;
+            if (x && answer > 1)
+            {
+                return $"{answer} dollars";
+            }
+
+            if (x && answer == 1)
+            {
+                return $"{answer} dollar";
+            }
+
+            return "";
+        }
+    }
+}
